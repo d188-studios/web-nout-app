ba1a4ca8ee8816c802ddab5939258bb48f2f8180
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-05-06 14:15:18 -0500

    Backup

--- src/features/editor/components/Content/Editor/Editor.tsx
+++ src/features/editor/components/Content/Editor/Editor.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 import { useEventEmitter } from '~/lib/eventemitter';
 import { Page } from '~/features/editor/types';
-import EditorJS from '@editorjs/editorjs';
+import EditorJS, { API } from '@editorjs/editorjs';
 import { EDITOR_JS_TOOLS } from './editorTools';
 
 export interface EditorProps {}
@@ -10,74 +10,132 @@ const BLOCK_PAGE_TITLE_ID = 'pageTitle';
 
 export function Editor(props: EditorProps) {
   const { addListener, emit } = useEventEmitter();
-  const ejsInstanceRef = useRef<EditorJS | null>(null);
+  const editorRef = useRef<EditorJS | null>(null);
+  const [page, setPage] = React.useState<Page | null>(null);
   const pageTitleRef = useRef<string | null>(null);
+
+  useLayoutEffect(() => {
+    if (editorRef.current) return;
+
+    editorRef.current = new EditorJS({
+      holder: 'editorjs',
+      tools: EDITOR_JS_TOOLS,
+    });
+  }, []);
 
   useEffect(() => {
     return addListener<Page>('openPage', (page) => {
-      if (ejsInstanceRef.current) {
-        ejsInstanceRef.current.destroy();
-        ejsInstanceRef.current = null;
-      }
+      setPage(page);
 
-      pageTitleRef.current = page.title;
+      if (!editorRef.current) return;
+      const editor = editorRef.current;
 
-      ejsInstanceRef.current = new EditorJS({
-        holder: 'editorjs',
-        tools: EDITOR_JS_TOOLS,
-        onChange: async (api) => {
-          const { blocks } = await api.saver.save();
+      // @ts-ignore
+      editor.configuration.onChange = async (api: API) => {
+        // const { blocks } = await api.saver.save();
 
-          const found = blocks.find(
-            (block) => block.id === BLOCK_PAGE_TITLE_ID
-          );
-          if (found) {
-            if (found.data.text !== pageTitleRef.current) {
-              pageTitleRef.current = found.data.text;
-              emit('updatePage', {
-                page,
-                editedPage: {
-                  title: found.data.text,
-                },
-              });
-            }
-          }
-        },
-        data: {
-          blocks: [
-            {
-              id: BLOCK_PAGE_TITLE_ID,
-              type: 'header',
-              data: {
-                text: page.title,
-                level: 1,
-              },
+        // const found = blocks.find((block) => block.id === BLOCK_PAGE_TITLE_ID);
+        // if (!found) return;
+
+        // emit('updatePage', {
+        //   page,
+        //   editedPage: {
+        //     title: found.data.text,
+        //   },
+        // });
+      };
+
+      editor.blocks.render({
+        blocks: [
+          {
+            id: BLOCK_PAGE_TITLE_ID,
+            type: 'header',
+            data: {
+              text: page.title,
+              level: 1,
             },
-          ],
-        },
+          },
+        ],
       });
     });
   }, [addListener, emit]);
+
+  // useEffect(() => {
+  //   return addListener<Page>('openPage', (page) => {
+  //     instanceCountRef.current += 1;
+  //     const instanceId = instanceCountRef.current;
+
+  //     // if (ejsInstanceRef.current && !loadingRef.current) {
+  //     //   ejsInstanceRef.current.destroy();
+  //     //   ejsInstanceRef.current = null;
+  //     // }
+
+  //     pageTitleRef.current = page.title;
+  // const newInstance = new EditorJS({
+  //   holder: 'editorjs',
+  //   tools: EDITOR_JS_TOOLS,
+  //   onReady: () => {
+  //     if(instanceId !== instanceCountRef.current)
+  //       newInstance.destroy();
+  //   },
+  //   onChange: async (api) => {
+  //     const { blocks } = await api.saver.save();
+
+  //         const found = blocks.find(
+  //           (block) => block.id === BLOCK_PAGE_TITLE_ID
+  //         );
+  //         if (found) {
+  //           if (found.data.text !== pageTitleRef.current) {
+  //             pageTitleRef.current = found.data.text;
+  //             emit('updatePage', {
+  //               page,
+  //               editedPage: {
+  //                 title: found.data.text,
+  //               },
+  //             });
+  //           }
+  //         }
+  //       },
+  //       data: {
+  //         blocks: [
+  //           {
+  //             id: BLOCK_PAGE_TITLE_ID,
+  //             type: 'header',
+  //             data: {
+  //               text: page.title,
+  //               level: 1,
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     });
+
+  //     ejsInstanceRef.current = newInstance;
+  //   });
+  // }, [addListener, emit]);
 
   useEffect(() => {
     const removeUpdatePageListener = addListener<Page>(
       'updateOpenPage',
       (updatedPage) => {
-        if (!ejsInstanceRef.current) return;
-        if (updatedPage.title === pageTitleRef.current) return;
+        setPage(updatedPage);
+        // if (!editorRef.current) return;
 
-        ejsInstanceRef.current.blocks.update(BLOCK_PAGE_TITLE_ID, {
-          text: updatedPage.title,
-          level: 1,
-        });
+        // if (pageTitleRef.current === updatedPage.title) return;
+        // console.log(pageTitleRef.current, updatedPage.title);
+        // pageTitleRef.current = updatedPage.title;
+
+        // editorRef.current.blocks.update(BLOCK_PAGE_TITLE_ID, {
+        //   text: updatedPage.title,
+        //   level: 1,
+        // });
       }
     );
 
     const removeDeletePageListener = addListener('deleteOpenPage', () => {
-      if (!ejsInstanceRef.current) return;
-
-      ejsInstanceRef.current.destroy();
-      ejsInstanceRef.current = null;
+      if (!editorRef.current) return;
+      setPage(null);
+      editorRef.current.blocks.clear();
     });
 
     return () => {
@@ -88,7 +146,13 @@ export function Editor(props: EditorProps) {
 
   return (
     <div className="flex-1 relative">
-      <div className="absolute inset-0 overflow-auto p-20" id="editorjs"></div>
+      <div
+        style={{
+          visibility: page ? 'visible' : 'hidden',
+        }}
+        className="absolute inset-0 overflow-auto p-20"
+        id="editorjs"
+      ></div>
     </div>
   );
 }
