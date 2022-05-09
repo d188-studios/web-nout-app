42a4ae2f001c86e165f491e73ce2ecc69a615db7
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-05-08 20:41:41 -0500

    Added helmet

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
 export const API_URL = process.env.REACT_APP_API_URL as string;
+export const APP_NAME = process.env.REACT_APP_NAME as string;
--- src/features/editor/components/Content/Content.tsx
+++ src/features/editor/components/Content/Content.tsx
@@ -1,9 +1,28 @@
 import { Main } from './Main';
 import { Header } from './Header';
+import { useParams } from 'react-router-dom';
+import { usePages } from '../../stores/pages';
+import { useMemo } from 'react';
+import { Helmet } from 'react-helmet';
+import { APP_NAME } from '~/config';
 
 export function Content() {
+  const { selectedPageId } = useParams();
+  const { findPage } = usePages();
+
+  const selectedPage = useMemo(() => {
+    if (!selectedPageId) return null;
+
+    return findPage(selectedPageId);
+  }, [findPage, selectedPageId]);
+
   return (
     <div className="flex-1 flex flex-col">
+      <Helmet>
+        <title>
+          {selectedPage ? `${selectedPage.title} - ${APP_NAME}` : APP_NAME}
+        </title>
+      </Helmet>
       <Header />
       <Main />
     </div>
