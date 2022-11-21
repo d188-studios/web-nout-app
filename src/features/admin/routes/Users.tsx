ff3f93612508f3b33d8ce80ebf99a6f5f3bff3db
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-11-20 21:57:32 -0600

    Complete users table filters

--- src/features/admin/routes/Users.tsx
+++ src/features/admin/routes/Users.tsx
@@ -1,8 +1,10 @@
 import {
   Button,
   Checkbox,
+  Select,
   Table,
   TablePaginationConfig,
+  Typography,
 } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { FilterValue, SorterResult } from 'antd/lib/table/interface';
@@ -20,6 +22,8 @@ interface DataType {
   username: string;
   email: string;
   baneado: boolean;
+  verificado: boolean;
+  encuesta_contestada: boolean;
   created_at: string;
 }
 
@@ -53,6 +57,22 @@ const columns: ColumnsType<DataType> = [
     },
   },
   {
+    title: 'Verificado',
+    dataIndex: 'verificado',
+    width: 100,
+    render: (_, row) => {
+      return <Checkbox disabled checked={row.verificado} />;
+    },
+  },
+  {
+    title: 'Encuesta contestada',
+    dataIndex: 'encuesta_contestada',
+    width: 100,
+    render: (_, row) => {
+      return <Checkbox disabled checked={row.encuesta_contestada} />;
+    },
+  },
+  {
     title: 'Fecha de creación',
     dataIndex: 'created_at',
     render: (_, row) => {
@@ -76,6 +96,67 @@ export const Users: React.FC = () => {
       pageSize: 10,
     },
   });
+  const [usernames, setUsernames] = useState<string[]>();
+  const [emails, setEmails] = useState<string[]>([]);
+  const [baneado, setBaneado] = useState<boolean | undefined>();
+  const [verificado, setVerificado] = useState<boolean | undefined>();
+  const [contestada, setContestada] = useState<boolean | undefined>();
+
+
+  const onChangeUsernames = useCallback((usernames: string[]) => {
+    setUsernames(usernames);
+    setTableParams((tableParams) => ({
+      ...tableParams,
+      pagination: {
+        ...tableParams.pagination,
+        current: 1,
+      },
+    }));
+  }, []);
+
+  const onChangeEmails = useCallback((emails: string[]) => {
+    setEmails(emails);
+    setTableParams((tableParams) => ({
+      ...tableParams,
+      pagination: {
+        ...tableParams.pagination,
+        current: 1,
+      },
+    }));
+  }, []);
+
+  const onChangeBaneado = useCallback((baneado: boolean | undefined) => {
+    setBaneado(baneado);
+    setTableParams((tableParams) => ({
+      ...tableParams,
+      pagination: {
+        ...tableParams.pagination,
+        current: 1,
+      },
+    }));
+  }, []);
+
+  const onChangeVerificado = useCallback((verificado: boolean | undefined) => {
+    setVerificado(verificado);
+    setTableParams((tableParams) => ({
+      ...tableParams,
+      pagination: {
+        ...tableParams.pagination,
+        current: 1,
+      },
+    }));
+  }, []);
+
+  const onChangeContestada = useCallback((contestada: boolean | undefined) => {
+    setContestada(contestada);
+    setTableParams((tableParams) => ({
+      ...tableParams,
+      pagination: {
+        ...tableParams.pagination,
+        current: 1,
+      },
+    }));
+  }, []);
 
   const getAll = useCallback(async () => {
     setLoading(true);
@@ -84,6 +165,11 @@ export const Users: React.FC = () => {
       const res = await webNout.post('/admin/users', {
         page: tableParams.pagination?.current,
         limit: tableParams.pagination?.pageSize,
+        usernames,
+        email: emails,
+        baneado,
+        contestada,
+        verificado
       });
 
       setData(res.data.users);
@@ -100,7 +186,7 @@ export const Users: React.FC = () => {
     }
 
     setLoading(false);
-  }, [JSON.stringify(tableParams)]);
+  }, [JSON.stringify(tableParams), usernames, emails, baneado, contestada, verificado]);
 
   useEffect(() => {
     getAll();
@@ -133,6 +219,7 @@ export const Users: React.FC = () => {
 
     try {
       await webNout.post('/admin/ban', rowSelection.selectedRowKeys);
+      setSelectedRowKeys([]);
       getAll();
     } catch (e) {
       console.error(e);
@@ -143,15 +230,109 @@ export const Users: React.FC = () => {
 
   return (
     <div className="p-8 pt-4">
-      <div className="flex flex-row items-center mb-6 h-8">
+      <div className="flex items-center mb-4 h-8">
         <div className="flex-1">
-          <BC title="Usuarios"/>
+          <BC title="Usuarios" />
         </div>
         {hasSelected ? (
           <Button loading={banning} onClick={banSelectedUsers} danger>
             Toggle Baneo
           </Button>
         ) : null}
+      </div>
+      <div className="flex mb-6">
+        <div className="flex flex-col flex-1 mr-4">
+          <div className="flex flex-col mb-4">
+            <Typography.Text className="mb-2">Baneado</Typography.Text>
+            <Select
+              className="flex-1"
+              placeholder="Filtrar por baneado"
+              allowClear
+              value={baneado}
+              onChange={onChangeBaneado}
+              options={[
+                {
+                  value: true,
+                  label: 'Baneado',
+                },
+                {
+                  value: false,
+                  label: 'No baneado',
+                },
+              ]}
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <Typography.Text className="mb-2">Verificado</Typography.Text>
+            <Select
+              className="flex-1"
+              placeholder="Filtrar por veridicado"
+              allowClear
+              value={verificado}
+              onChange={onChangeVerificado}
+              options={[
+                {
+                  value: true,
+                  label: 'Verificado',
+                },
+                {
+                  value: false,
+                  label: 'No verificado',
+                },
+              ]}
+            />
+          </div>
+          <div className="flex flex-col">
+            <Typography.Text className="mb-2">
+              Encuesta contestada
+            </Typography.Text>
+            <Select
+              className="flex-1"
+              placeholder="Filtrar por encuesta contestada"
+              allowClear
+              value={contestada}
+              onChange={onChangeContestada}
+              options={[
+                {
+                  value: true,
+                  label: 'Contestada',
+                },
+                {
+                  value: false,
+                  label: 'No contestada',
+                },
+              ]}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex flex-col mb-4">
+            <Typography.Text className="mb-2">
+              Nombres de usuario
+            </Typography.Text>
+            <Select
+              className="flex-1"
+              mode="tags"
+              placeholder="Filtrar por nombres de usuario"
+              allowClear
+              value={usernames}
+              onChange={onChangeUsernames}
+            />
+          </div>
+          <div className="flex flex-col">
+            <Typography.Text className="mb-2">
+              Correos electronicos
+            </Typography.Text>
+            <Select
+              className="flex-1"
+              mode="tags"
+              placeholder="Filtrar por correos electronicos"
+              allowClear
+              value={emails}
+              onChange={onChangeEmails}
+            />
+          </div>
+        </div>
       </div>
       <Table
         rowKey={(row) => row.id}
@@ -163,7 +344,7 @@ export const Users: React.FC = () => {
         onChange={handleTableChange}
         size="small"
         scroll={{
-          y: 600,
+          y: 500,
         }}
       />
     </div>
